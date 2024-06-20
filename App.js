5d585e9829a90a9b708fe7449e60a2e4b1bb90a2
nihadkalathil <117640897+nihadkalathil@users.noreply.github.com>
2024-06-20 11:38:46 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Content from './Content';
 import Header from './Header';
+import Project from './Project';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
 <Content name="Nihad"> </Content>
+    <Project favcol="green"></Project>
     </div>
   );
 }
